ddde4b656ad740f8decf4792eed9117662006d75
tugane <tooamiel@gmail.com>
2022-09-27 17:30:31 +0200

    Add field deprecation

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,9 +40,12 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
+    durationInSeconds: ({ length }) => length,
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
+    },
+    Module: {
+      durationInSeconds: ({ length }) => length,
     },
   },
 };
